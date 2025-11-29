969b06f667a5c4b04609a1370d542dde70d18d26
ShiraiMizuki <shirai110409@gmail.com>
2025-11-30 00:20:35 +0100

    so the whole skeleton of the app was added its kidna working but it lacks a lot

--- app/src/main/java/com/example/bandytabingeprojekt/Quiz.java
+++ app/src/main/java/com/example/bandytabingeprojekt/Quiz.java
@@ -1,24 +1,239 @@
 package com.example.bandytabingeprojekt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Quiz extends AppCompatActivity {
+    Random random = new Random();
+    int aktualnepytanie;
+    Button odp1, odp2, odp3, odp4;
+    TextView pytanie , ktorepytanie;
+    int punkty , licznikpytan=1;
+    HashMap<Integer, List<String>> pytaniaodp = new HashMap<>();
+    String wartoscp , poprawne;
+
+    void losulosu() {
+         aktualnepytanie = random.nextInt(20) + 1;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_quiz);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            //STRUKTURA LISTY 0 PYTANIE | 1 ODP | 2 ODP | 3 ODP | 4 ODP | 5 POPRAWNA NUMER
+            pytaniaodp.put(1, Arrays.asList("Brat zbił bombkę – rodzice mogą się wkurzyć. Co robisz?", "Krzyczysz dyskretnie i sprzątasz sam."
+                    ,"Pomagasz, pocieszasz i uczysz ostrożności."
+                    ,"Śmiejesz się, robisz foto na pamiątkę."
+                    , "Obwin. przed rodzicami, ale nieumyślne."
+                    , "2"));
+            pytaniaodp.put(2, Arrays.asList("Prezent nie w gust – darczyńca się starał. Jak reagujesz?", "Rzucasz w kąt po kolacji."
+                    ,"Mówisz szczerze i propon. wymianę."
+                    ,"Dziękujesz, doceniasz, później podarujesz."
+                    , "Ukrywasz i udaj. entuzjazm."
+                    , "3"));
+
+            pytaniaodp.put(3, Arrays.asList("Resztki po wigilii, jesteś zmęczony. Co z nimi?", "Rzucasz w rodzeństwo na żarty."
+                    ,"Wyrzucasz szybko do śmieci."
+                    ,"Zostaw. bałagan rodzicom."
+                    , "Sprzątasz, pakujesz dla potrzebujących."
+                    , "4"));
+
+            pytaniaodp.put(4, Arrays.asList("Elf ma problem z pakowaniem – opóźni to twoją zabawę?", "Pomagasz, pakowac razem z radością."
+                    ,"Śmiej. się, daj. szybką radę."
+                    ,"Kradniesz dyskretnie jeden prezent."
+                    , "Przeszkadz. lekko, potem przeprasz."
+                    , "1"));
+
+            pytaniaodp.put(5, Arrays.asList("Zwierzę przestraszone hałasem, kumple chcą głośno? ", "Gonisz je dla zabawy."
+                    ,"Dbasz, dajesz smakołyki, chronisz przed hałasem."
+                    ,"Daj. resztki, co mogą zaszkodzić."
+                    , "Ignoruj ,jestes potrzebny w zabawie."
+                    , "2"));
+
+            pytaniaodp.put(6, Arrays.asList("Ktoś smutny w rodzinie – co robisz? ", "Mów. nie psuj zabawy."
+                    ,"Śmiej. się z smutku."
+                    ,"Rozmawiaj, rozweselaj."
+                    , "Unikasz i baw. się sam."
+                    , "3"));
+
+            pytaniaodp.put(7, Arrays.asList("Przygot. do Mikołaja – co robisz cały rok? ", "Podgląd. prezenty."
+                    ,"Kłam o zachowaniu."
+                    ,"Wymuszaj lepsze prezenty."
+                    , "Jesteś grzeczny, pomagasz w domu."
+                    , "4"));
+
+            pytaniaodp.put(8, Arrays.asList("Choinka po świętach – co z nią? ", "Demontaż i recykling."
+                    ,"Zostaw. igły, nie sprząt."
+                    ,"Niszcz ozdoby."
+                    , "Narzekaj strata czasu."
+                    , "1"));
+
+            pytaniaodp.put(9, Arrays.asList("Przegrywasz grę z kumplami – reakcja?", "Wściek. się, wychodz."
+                    ,"Gratuluj, proponuj. rewanż."
+                    ,"Oszukaj następnym razem."
+                    , "Obwin innych."
+                    , "2"));
+
+            pytaniaodp.put(10, Arrays.asList("Bezdomny w święta – co robisz? ", "Omijin szerokim łukiem."
+                    ,"Śmiej się z nim."
+                    ,"Daj mu posiłek lub datek."
+                    , "Fotografuj dla lajków."
+                    , "3"));
+
+            pytaniaodp.put(11, Arrays.asList("1. Dzielisz prezenty z rodzeństwem? ", "Nie dzielisz się zabawkami."
+                    ,"Bierzesz ich rzeczy bez pytania."
+                    ,"Wymuszasz ich rzeczy."
+                    , "Dajesz coś od siebie."
+                    , "4"));
+            pytaniaodp.put(12, Arrays.asList("Kolędowanie z rodziną – co robisz? ", "Śpiewasz radośnie."
+                    ,"Śmiejesz się z fałszujących."
+                    ,"Udajesz, że nie znasz tekstu."
+                    , "Przerywasz dla żartu."
+                    , "1"));
+            pytaniaodp.put(13, Arrays.asList("Za dużo słodyczy – co z nimi? ", "Zjadasz sam w ukryciu."
+                    ,"Dzielisz się z innymi."
+                    ,"Rzucasz w innych."
+                    , "Chowasz i milczysz."
+                    , "2"));
+            pytaniaodp.put(14, Arrays.asList("Fajerwerki straszą zwierzaki? ", "Śmiejesz się z ich strachu."
+                    ,"Puszczasz więcej."
+                    ,"Uspokajasz, chronisz."
+                    , "Ignorujesz i patrzysz."
+                    , "3"));
+            pytaniaodp.put(15, Arrays.asList("Kolega zapomniał prezent w szatni? ", "Zabierasz dla siebie."
+                    ,"Otwierasz, żeby sprawdzić."
+                    ,"Mówisz wszystkim dla śmiechu."
+                    , "Oddajesz dyskretnie."
+                    , "4"));
+            pytaniaodp.put(16, Arrays.asList("Dekorowanie choinki – twoja rola? ", "Wieszasz ozdoby, dzielisz się miejscem."
+                    ,"Wieszasz tylko na górze."
+                    ,"Zbijasz „przypadkiem”."
+                    , "Patrzysz, jak inni dekorują."
+                    , "1"));
+            pytaniaodp.put(17, Arrays.asList("Sanie Mikołaja w zaspie? ", "Śmiejesz się i nagrywasz."
+                    ,"Popychasz razem z elfami."
+                    ,"Podkładasz głaz."
+                    , "Czekasz na innych."
+                    , "2"));
+            pytaniaodp.put(18, Arrays.asList("Przyjaciel przeprasza za psikus? ", "Nie wybaczasz, złościsz się."
+                    ,"Mścisz się."
+                    ,"Wybaczasz, śmiejecie się razem."
+                    , "Opowiadasz wszystkim."
+                    , "3"));
+            pytaniaodp.put(19, Arrays.asList("Papier po prezentach – co z nim? ", "Rzucasz na podłogę."
+                    ,"Robisz kulki i zaczynasz walkę."
+                    ,"Zostawiasz to mamie."
+                    , "Zbierasz i wyrzucasz do kosza."
+                    , "4"));
+            pytaniaodp.put(20, Arrays.asList("Życzenia świąteczne – jak składasz? ", "Szczerze, z serca."
+                    ,"Meh, wesołych"
+                    ,"Śmieszne lub obraźliwe."
+                    , "Kopiujesz z internetu."
+                    , "1"));
+             odp1 = findViewById(R.id.Odp1);
+             odp2 = findViewById(R.id.Odp2);
+             odp3 = findViewById(R.id.Odp3);
+             odp4 = findViewById(R.id.Odp4);
+            pytanie = findViewById(R.id.pytanie);
+            ktorepytanie = findViewById(R.id.ktorepytanie);
+            ktorepytanie.setText(licznikpytan+"/10");
+            updatebuttons();
+// i know that i could do it more effectivly but im too lazy and it looks funny
             return insets;
+
         });
+
+
     }
-}
+
+    void updatebuttons(){
+        losulosu();
+        System.out.println(aktualnepytanie);
+        List<String> dane = pytaniaodp.get(aktualnepytanie);
+        String p = dane.get(0);
+        String o1 = dane.get(1);
+        String o2 = dane.get(2);
+        String o3 = dane.get(3);
+        String o4 = dane.get(4);
+         poprawne = dane.get(5);
+        odp1.setText(o1);
+        odp2.setText(o2);
+        odp3.setText(o3);
+        odp4.setText(o4);
+        pytanie.setText(p);
+        odp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wartoscp = "1";
+                sprawdzczyprawidlowa();
+            }
+        });
+        odp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wartoscp = "2";
+                sprawdzczyprawidlowa();
+            }
+        });
+        odp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wartoscp = "3";
+                sprawdzczyprawidlowa();
+            }
+        });
+        odp4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wartoscp = "4";
+                sprawdzczyprawidlowa();
+            }
+
+        });
+
+    }
+       void sprawdzczyprawidlowa(){
+        if(licznikpytan > 9 ){
+            int finalpunkty =punkty;
+            if(punkty >3){
+                Intent intent = new Intent(Quiz.this , zdane.class);
+                intent.putExtra("punkty", punkty);
+                startActivity(intent);
+            }else {
+                Intent intent = new Intent(Quiz.this , niezdane.class);
+                startActivity(intent);
+            }
+        }
+              if(wartoscp == poprawne){
+                punkty++;
+                  updatebuttons();
+                  licznikpytan++;
+                  ktorepytanie.setText(licznikpytan+"/10"+"e");
+              }else {
+                  updatebuttons();
+                  licznikpytan++;
+                  ktorepytanie.setText(licznikpytan+"/10");
+              }
+       }
+    }
+
--- app/src/main/java/com/example/bandytabingeprojekt/zdane.java
+++ app/src/main/java/com/example/bandytabingeprojekt/zdane.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,35 +12,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class zdane extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_zdane);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            Button Start = findViewById(R.id.startb);
-            Button Quit = findViewById(R.id.Quitb);
-
-
-            Start.setOnClickListener(new View.OnClickListener() {
+            Button powrot = findViewById(R.id.powrot);
+                    TextView punktey = findViewById(R.id.punkty);
+                    Intent intent = getIntent();
+                    int punkty = intent.getIntExtra("punkty", 0);
+                    punktey.setText("punkty:"+punkty);
+            powrot.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this , Quiz.class);
+                    Intent intent = new Intent(zdane.this , MainActivity.class);
                     startActivity(intent);
                 }
-            });
-            Quit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finishAffinity();
-                    System.exit(0);
-                }
-            });
-            return insets;
+
         });
+            return insets;
     }
-}
+        );}}
